e9ff6f3a2d7a118cbfdc2c96b34c48521eb74f67
laleth <trlaleth@gmail.com>
2023-07-04 19:18:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+
+        let timer=10;
+        setTimeout(() => {
+            document.getElementById("timer").innerHTML = timer--;
+            setTimeout(() => {
+                document.getElementById("timer").innerHTML = timer--;
+                setTimeout(() => {
+                    document.getElementById("timer").innerHTML = timer--;
+                    setTimeout(() => {
+                        document.getElementById("timer").innerHTML = timer--;
+                        setTimeout(() => {
+                            document.getElementById("timer").innerHTML = timer--;
+                            setTimeout(() => {
+                                document.getElementById("timer").innerHTML = timer--;
+                                setTimeout(() => {
+                                    document.getElementById("timer").innerHTML = timer--;
+                                    setTimeout(() => {
+                                        document.getElementById("timer").innerHTML = timer--;
+                                        setTimeout(() => {
+                                            document.getElementById("timer").innerHTML = timer--;
+                                            setTimeout(() => {
+                                                document.getElementById("timer").innerHTML = timer--;
+                                                setTimeout(() => {
+                                                    document.getElementById("timer").innerHTML = "Happy Independence Day";
+
+                                                }, 1000)
+                                            }, 1000)
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        })
+ 
